7d3b831b8ac84c9cc58eaf0dd37c0248c8f79858
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2024-10-05 12:07:20 -0700

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -9,50 +10,27 @@ import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(600);
-        RoadRunnerBotEntity blueTop = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(53, 53, Math.toRadians(180), Math.toRadians(180), 12.3)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-60, 10, 0))
-                                .forward(30)
-                                .waitSeconds(1)
-                                .turn(Math.toRadians(90))
-                                .strafeRight(10)
-                                .waitSeconds(1)
-                                .strafeLeft(30)
-                                .forward(30)
-                                .strafeRight(20)
-                                .waitSeconds(1)
-                                .strafeRight(5)
-                                .waitSeconds(1)
-                                .strafeRight(5)
-                                .build()
-                );
         RoadRunnerBotEntity redTop = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(53, 53, Math.toRadians(180), Math.toRadians(180), 12.3)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(10, -70, Math.toRadians(90)))
-                                .forward(30)
-                                .waitSeconds(1)
-                                .turn(Math.toRadians(-90))
-                                .strafeLeft(10)
-                                .waitSeconds(1)
-                                .strafeRight(10)
-                                .turn(Math.toRadians(-180))
-                                .waitSeconds(1)
-                                .strafeRight(10)
-                                .strafeLeft(30)
-                                .turn(Math.toRadians(-180))
-                                .forward(30)
-                                .strafeLeft(20)
-                                .waitSeconds(1)
-                                .strafeLeft(5)
-                                .waitSeconds(1)
-                                .strafeLeft(5)
-                                .waitSeconds(1)
-                                .strafeLeft(90)
-                                .back(95)
+                        drive.trajectorySequenceBuilder(new Pose2d(-26, -55, Math.toRadians(90)))
+                                //moving to basket area
+                                .splineTo(new Vector2d(-58.5, -46.2), Math.toRadians(300))
+                                //grabbing first sample and bringing it onto vertical slide
+                                .waitSeconds(3)
+                                //turn to check second sample is present
+                                .turn(Math.toRadians(-30))
+                                //Placing first sample in basket after turning and then grabbing
+                                //second sample and placing second sample in basket
+                                .waitSeconds(5)
+                                //move to spot to grab third sample and be ready to quickly move to
+                                //substation to get human player ready for clipping sample
+                                .splineTo(new Vector2d(-36,-35), Math.toRadians(320))
+                                // grabbing third sample
+                                .waitSeconds(3)
+                                //moving to position for taking a piece from the submersible
+                                .splineTo(new Vector2d(23, -46), Math.toRadians(140))
                                 .build()
                 );
 
