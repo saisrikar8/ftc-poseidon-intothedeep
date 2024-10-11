c445e07b30ad1f351e75e361f9f097a6486f913c
yixhen <143676566+l1ionelw@users.noreply.github.com>
2024-10-11 10:42:57 -0700

    test origin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ChassisTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ChassisTesting.java
@@ -19,7 +19,8 @@ import org.firstinspires.ftc.teamcode.MecanumDrive.TurnAction;
 @Autonomous(name = "Chassis Testing")
 public class ChassisTesting extends LinearOpMode {
     MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-    public void runOpMode(){
+
+    public void runOpMode() {
         waitForStart();
         TrajectoryActionBuilder traj1 = null;
         TrajectoryActionBuilder traj2 = null;
@@ -27,21 +28,21 @@ public class ChassisTesting extends LinearOpMode {
         TrajectoryActionBuilder traj4 = null;
         TrajectoryActionBuilder traj5 = null;
         TrajectoryActionBuilder traj6 = null;
-        while(!isStopRequested() && !opModeIsActive()){
+        while (!isStopRequested() && !opModeIsActive()) {
             // init code here
-            traj1 = drive.actionBuilder(new Pose2d(0,0,0))
+            traj1 = drive.actionBuilder(new Pose2d(0, 0, 0))
                     .strafeTo(new Vector2d(10, 0));
 
             traj2 = drive.actionBuilder(new Pose2d(10, 0, 0))
                     .strafeTo(new Vector2d(-10, 0));
 
             traj3 = drive.actionBuilder(new Pose2d(-10, 0, 0))
-                    .strafeTo(new Vector2d(0,0));
+                    .strafeTo(new Vector2d(0, 0));
 
-            traj4 = drive.actionBuilder(new Pose2d(0,0,0))
+            traj4 = drive.actionBuilder(new Pose2d(0, 0, 0))
                     .splineTo(new Vector2d(10, 10), 0);
 
-            traj5 = drive.actionBuilder(new Pose2d(10,10,0))
+            traj5 = drive.actionBuilder(new Pose2d(10, 10, 0))
                     .splineTo(new Vector2d(-10, -10), 0);
 
             traj6 = drive.actionBuilder(new Pose2d(-10, -10, 0))
