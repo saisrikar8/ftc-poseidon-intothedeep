c2aa7abe53280727689349a427f66508f30a9c40
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2024-11-11 15:17:54 -0800

    Updating some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -68,8 +68,8 @@ public final class MecanumDrive {
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 1.4464323710111469;
+        public double kV = 0.004407837282680679;
         public double kA = 0;
 
         // path profile parameters (in inches)
