f7426b8a9c352ecc120d7bae4f87c91c163231ce
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2025-01-20 08:57:47 -0800

    Testing the new Claw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ClawTesting.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Claw Testing")
+public class ClawTesting extends LinearOpMode {
+    Servo horizontalClaw, horizontalClawRotator, horizontalClawRotator2;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        horizontalClaw = hardwareMap.get(Servo.class, "horizontal-claw");
+        horizontalClawRotator = hardwareMap.get(Servo.class, "horizontal-claw-rotator");
+        horizontalClawRotator2 = hardwareMap.get(Servo.class, "horizontal-claw-rotator-2");
+
+        waitForStart();
+        while (opModeIsActive() && !isStopRequested()){
+            horizontalClawRotator.scaleRange(-0.5, 1.5);
+            horizontalClawRotator2.scaleRange(-0.5,1.5);
+            horizontalClaw.setPosition(filterPosition(horizontalClaw.getPosition()+gamepad1.right_stick_x/1000));
+            horizontalClawRotator.setPosition(filterPosition(horizontalClawRotator.getPosition()+gamepad1.left_stick_y/1000));
+            horizontalClawRotator2.setPosition(filterPosition(horizontalClawRotator2.getPosition() + gamepad1.left_stick_x/1000));
+        }
+    }
+
+    public double filterPosition(double position){
+        return Math.max(0, Math.min(1, position));
+    }
+}
