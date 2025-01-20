3e0798b88711932215ef40595dfc1e47f3c7c82f
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2025-01-20 10:56:10 -0800

    OpMode to test the pipelines

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SampleDetection.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.pipelines.SampleDetectionPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+
+@TeleOp(name = "Sample Detection")
+public class SampleDetection extends LinearOpMode {
+    OpenCvPipeline pipeline;
+    OpenCvCamera camera;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        pipeline = new SampleDetectionPipeline();
+
+        camera.setPipeline(pipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                // Once the camera is opened, start streaming
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                // Handle error
+                telemetry.addData("Camera Error", "Error Code: " + errorCode);
+                telemetry.update();
+            }
+        });
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()){
+            telemetry.addData("FPS", camera.getFps());
+            telemetry.addData("Pipeline runtime", camera.getPipelineTimeMs());
+            telemetry.addData("Frame count", camera.getFrameCount());
+            telemetry.update();
+        }
+    }
+}
