496d0d0b0a1c7c02930b9952799c535605568bf4
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2025-01-20 16:17:58 -0800

    OpMode to test the pipelines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -8,4 +8,9 @@ public class Constants {
     public static final double SLIDE_TICKS_PER_INCH = 113.64;
 
     public static final int MAX_SLIDE_EXTENSION = -3740;
+    public static final double HORIZONTAL_CLAW_OPEN_POS = 0.7594;
+    public static final double HORIZONTAL_CLAW_CLOSE_POS = 0.4717;
+    public static final double HORIZONTAL_CLAW_PICKUP_POS_PITCH = 0.5894;
+    public static final double HORIZONTAL_CLAW_TRANSFER_POS_PITCH = 0;
+    public static final double HORIZONTAL_CLAW_IDLE_YAW = 0.3122;
 }
