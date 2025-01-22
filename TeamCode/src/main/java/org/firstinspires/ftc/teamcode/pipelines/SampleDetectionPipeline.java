8a72741447a4cc118d056e252b1df631b0e85bd6
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2025-01-21 18:08:28 -0800

    OpMode to test the pipelines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/SampleDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/SampleDetectionPipeline.java
@@ -6,6 +6,7 @@ import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
@@ -13,51 +14,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SampleDetectionPipeline extends OpenCvPipeline {
+    private Mat grayed = new Mat();
+    private Mat blurred = new Mat();
+    private Mat edges = new Mat();
     @Override
     public Mat processFrame(Mat input) {
-        EdgeDetectionPipeline edgeDetectionPipeline = new EdgeDetectionPipeline();
-        Mat edges = edgeDetectionPipeline.processFrame(input);
+        if (input == null || input.empty()) {
+            return null; // Safeguard against undefined input Mat
+        }
+        Imgproc.cvtColor(input, grayed, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.GaussianBlur(grayed, blurred, new Size(5,5), 0);
+        double lowerThreshold = 50;
+        double upperThreshold = 150;
+
+        Imgproc.Canny(blurred, edges, lowerThreshold, upperThreshold);
+        // Find contours from the binary edge image
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        Point imageCenter = new Point(edges.width() / 2, edges.height() / 2);
-
-        double minDistance = Double.MAX_VALUE;
-        RotatedRect closestRect = null;
-
+        // Process each contour
         for (MatOfPoint contour : contours) {
-            if (Imgproc.contourArea(contour) > 500) {
+            // Calculate the minimum area rectangle
+            if (Imgproc.contourArea(contour) > 450) { // Filter by minimum area
                 RotatedRect minRect = Imgproc.minAreaRect(new MatOfPoint2f(contour.toArray()));
 
-                Point rectCenter = minRect.center;
-
-                double distance = Math.sqrt(Math.pow(rectCenter.x - imageCenter.x, 2) + Math.pow(rectCenter.y - imageCenter.y, 2));
-
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    closestRect = minRect;
+                // Get the angle and adjust if needed
+                double angle = minRect.angle;
+                if (minRect.size.width < minRect.size.height) {
+                    angle += 90;
                 }
+
+                // Get the points of the rectangle
+                Point[] points = new Point[4];
+                minRect.points(points);
+
+                // Draw the rectangle
+                for (int i = 0; i < 4; i++) {
+                    Imgproc.line(input, points[i], points[(i + 1) % 4], new Scalar(0, 255, 0), 2);
+                }
+
+                // Draw the angle text
+                Imgproc.putText(input, "Angle: " + angle, new Point(minRect.center.x - 50, minRect.center.y),
+                        Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 255, 0), 2);
             }
         }
 
-        if (closestRect != null) {
-            Point[] points = new Point[4];
-            closestRect.points(points);
-
-            for (int i = 0; i < 4; i++) {
-                Imgproc.line(edges, points[i], points[(i + 1) % 4], new Scalar(0, 255, 0), 2);
-            }
-
-            double angle = closestRect.angle;
-            if (closestRect.size.width < closestRect.size.height) {
-                angle += 90;
-            }
-
-            Imgproc.putText(edges, "Angle: " + angle, new Point(closestRect.center.x - 50, closestRect.center.y),
-                    Imgproc.FONT_HERSHEY_SIMPLEX, 1, new Scalar(0, 255, 0), 2);
-        }
-
-        return edges;
+        // Return the processed image with contours and rectangles drawn
+        return input;
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/CameraTesting.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.pipelines.SampleDetectionPipeline;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+
+@TeleOp(name = "camera testing")
+public class CameraTesting extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+        VisionPortal portal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(1280, 720))
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
+                .enableLiveView(true)
+                .setAutoStartStreamOnBuild(true)
+                .build();
+
+        try {
+            waitForStart();
+
+            while (opModeIsActive() && !isStopRequested()) {
+                telemetry.addData("camera state", portal.getCameraState());
+                telemetry.addData("fps", portal.getFps());
+                telemetry.update();
+            }
+        } finally {
+            portal.close(); // Ensure the portal is closed when the OpMode ends
+        }
+    }
+
+}
