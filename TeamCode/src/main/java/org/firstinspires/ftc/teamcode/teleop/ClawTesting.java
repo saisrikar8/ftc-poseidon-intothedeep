bd8b32e328f9e07f2a564c9917ee26feebc7f6c5
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2025-01-29 20:46:22 -0800

    Adding pipeline and finetuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ClawTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ClawTesting.java
@@ -4,47 +4,25 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.Constants;
-
 @TeleOp(name = "Claw Testing")
 public class ClawTesting extends LinearOpMode {
-    Servo horizontalClaw, horizontalClawRotator, horizontalClawRotator2, armRotator;
+    Servo horizontalClaw, horizontalClawRotator, horizontalClawRotator2;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        horizontalClaw = hardwareMap.get(Servo.class, "horizontal-claw"); // open claw
-        horizontalClawRotator = hardwareMap.get(Servo.class, "horizontal-claw-rotator"); // pitch
-        horizontalClawRotator2 = hardwareMap.get(Servo.class, "horizontal-claw-rotator-2"); // yaw
-        armRotator = hardwareMap.get(Servo.class, "arm-rotator");
+        horizontalClaw = hardwareMap.get(Servo.class, "horizontal-claw");
+        horizontalClawRotator = hardwareMap.get(Servo.class, "horizontal-claw-rotator");
+        horizontalClawRotator2 = hardwareMap.get(Servo.class, "horizontal-claw-rotator-2");
 
         waitForStart();
-        while (opModeIsActive() && !isStopRequested()) {
-            if (gamepad2.a) {
-                horizontalClaw.setPosition(Constants.HORIZONTAL_CLAW_OPEN_POS);
-            }
-            if (gamepad2.b) {
-                horizontalClaw.setPosition(Constants.HORIZONTAL_CLAW_CLOSE_POS);
-            }
-            if (gamepad2.x) {
-                horizontalClawRotator2.setPosition(Constants.HORIZONTAL_CLAW_IDLE_YAW);
-                horizontalClawRotator.setPosition(Constants.HORIZONTAL_CLAW_PICKUP_POS_PITCH);
-            }
-            if (gamepad2.y) {
-                horizontalClawRotator2.setPosition(Constants.HORIZONTAL_CLAW_IDLE_YAW);
-                horizontalClawRotator.setPosition(Constants.HORIZONTAL_CLAW_TRANSFER_POS_PITCH);
-            }
-            if (gamepad2.left_bumper) {
-
-            }
-            horizontalClawRotator2.setPosition(horizontalClawRotator2.getPosition() + gamepad2.left_stick_x / 500);
-            telemetry.addData("open/close", horizontalClaw.getPosition());
-            telemetry.addData("pitch", horizontalClawRotator.getPosition());
-            telemetry.addData("yaw", horizontalClawRotator2.getPosition());
-            telemetry.update();
+        while (opModeIsActive() && !isStopRequested()){
+            horizontalClaw.setPosition(filterPosition(horizontalClaw.getPosition()+gamepad1.right_stick_x/1000));
+            horizontalClawRotator.setPosition(filterPosition(horizontalClawRotator.getPosition()+gamepad1.left_stick_y/1000));
+            horizontalClawRotator2.setPosition(filterPosition(horizontalClawRotator2.getPosition() + gamepad1.left_stick_x/1000));
         }
     }
 
-    public double filterPosition(double position) {
+    public double filterPosition(double position){
         return Math.max(0, Math.min(1, position));
     }
 }
