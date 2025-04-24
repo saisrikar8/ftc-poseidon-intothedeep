34db581222a2605a730d64a26fadf5a71823d799
saisrikar8 <77305508+saisrikar8@users.noreply.github.com>
2025-04-24 14:07:20 -0700

    update shooter bot demo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ShooterBotDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ShooterBotDemo.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 
 @TeleOp(name = "Shooter Bot Demo")
 public class ShooterBotDemo extends LinearOpMode {
@@ -13,40 +15,42 @@ public class ShooterBotDemo extends LinearOpMode {
     public DcMotor slide1, slide2;
 
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
         frontLeft = hardwareMap.get(DcMotor.class, "front-left");
         frontRight = hardwareMap.get(DcMotor.class, "front-right");
         backLeft = hardwareMap.get(DcMotor.class, "rear-left");
         backRight = hardwareMap.get(DcMotor.class, "rear-right");
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        shooter = hardwareMap.get(DcMotor.class, "shooter");
+        // shooter = hardwareMap.get(DcMotor.class, "shooter");
 
-        slide1 = hardwareMap.get(DcMotor.class, "slide-1");
-        slide2 = hardwareMap.get(DcMotor.class, "slide-2");
+        slide1 = hardwareMap.get(DcMotor.class, "horizontal-slide-1");
+        slide2 = hardwareMap.get(DcMotor.class, "horizontal-slide-2");
 
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // shooter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         slide1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slide2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         slide1.setDirection(DcMotorSimple.Direction.REVERSE);
         slide2.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        while (opModeIsActive() && !isStopRequested()){
-            moveRobot(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad2.right_stick_x);
+        waitForStart();
+        while (opModeIsActive() && !isStopRequested()) {
+            moveRobot(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
             slide1.setPower(gamepad1.right_stick_y);
             slide2.setPower(gamepad1.right_stick_y);
-            if (gamepad1.a){
-                shooter.setPower(1);
-            }
-            if (gamepad1.b) {
-                shooter.setPower(0);
-            }
+//            if (gamepad1.left_bumper) {
+//                shooter.setPower(0);
+//            }
+//            if (gamepad1.right_bumper) {
+//                shooter.setPower(0.6);
+//            }
         }
     }
 
